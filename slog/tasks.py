7d3f454124d253e462c00d4bd3f42b698b5e2567
benmcnelly <benmcnelly@gmail.com>
2017-02-15 17:10:34 -0600

    automated push from terminal

---
+++ slog/tasks.py
@@ -0,0 +1,12 @@
+from invoke import task, run
+
+@task
+def push(ctx):
+    ctx.run('echo UNGH!''')
+    ctx.run('wintersmith build')
+    ctx.run('cp -R build/* ../')
+    ctx.run('cp ../articles/now/index.html ../now.html')
+    ctx.run('git add -A')
+    ctx.run('git commit -m "automated push from terminal" ')
+    ctx.run('git push -f origin master')
+    ctx.run('firefox http://github.benmcnelly.com')
