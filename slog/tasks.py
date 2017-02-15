db3a466d682af2b02fbaaf6d25f081b9d897b887
benmcnelly <benmcnelly@gmail.com>
2017-02-15 17:35:34 -0600

    automated push from terminal

--- slog/tasks.py
+++ slog/tasks.py
@@ -6,6 +6,7 @@ def push(ctx):
     ctx.run('wintersmith build')
     ctx.run('cp -R build/* ../')
     ctx.run('cp ../articles/now/index.html ../now.html')
+    ctx.run('cd ..')
     ctx.run('git add -A')
     ctx.run('git commit -m "automated push from terminal" ')
     ctx.run('git push -f origin master')
